dd37e6ed5f453a177a4926946956bf7c3788a146
Mark Ahrens <mark-ahrens@uiowa.edu>
2016-03-08 16:08:00 -0600

    Continuing building out the local event pages

--- includes/uiowa-events-single-event.tpl.php
+++ includes/uiowa-events-single-event.tpl.php
@@ -10,11 +10,36 @@
  *
  * $title
  *   Sets the page title
- * $list
+ * $data
  *   Pre-formatted list.
  */
-$breadcrumb = array();
-$breadcrumb[] = l('Home', '<front>');
-$breadcrumb[] = l('Events', 'events');
-drupal_set_breadcrumb($breadcrumb);
+
+
+dpm($data);
 ?>
+<div id="uiowa-event">
+  <div id="main-column">
+    <div class="event-time">
+      <?php print $data['today'] ?>
+    </div>
+    <?php if (!empty($data['location_name'])): ?>
+      <div class="event-location"><?php print $data['location_name'] ?><?php if (!empty($data['room_number'])): ?><?php print ', '.$data['room_number'] ?>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+    <?php if (!empty($data['description'])): ?>
+      <div class="event-description">
+        <?php print $data['description'] ?>
+      </div>
+    <?php endif; ?>
+  </div>
+  <div id="secondary-column">
+    <?php
+      print theme('imagecache_external', array(
+        'path' => $data['photo_url'],
+        'style_name'=> 'medium',
+        'alt' => 'Druplicon',
+      ));
+    ?>
+  </div>
+</div>
